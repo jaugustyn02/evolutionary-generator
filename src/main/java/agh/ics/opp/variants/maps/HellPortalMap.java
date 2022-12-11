6120ed9654163361e2cf7e323881e3ae277dd4fb
Green0x0y <66325429+Green0x0y@users.noreply.github.com>
2022-12-11 22:57:40 +0100

    Update HellPortalMap

--- src/main/java/agh/ics/opp/variants/maps/HellPortalMap.java
+++ src/main/java/agh/ics/opp/variants/maps/HellPortalMap.java
@@ -5,19 +5,33 @@ import agh.ics.opp.SimulationSetup;
 import agh.ics.opp.Vector2d;
 
 public class HellPortalMap extends AbstractWorldMap{
-
+    
+    private final int portalEnergyConsumption;
+    
     public HellPortalMap(SimulationSetup setup) {
         super(new Vector2d(setup.mapWidth(), setup.mapHeight()));
+        this.portalEnergyConsumption = setup.animalEnergyConsumption();
     }
 
     @Override
     public boolean canMoveTo(Vector2d position) {
-        return true;
+        return  (! (objectAt(position) instanceof Animal));
     }
 
     @Override
     public void correctAnimalPosition(Vector2d oldPosition, Animal animal) {
-        System.out.println("not this one yet");
+        if( overBorder(animal.getPosition())) {
+                animal.setPosition(Vector2d.getRandom(super.getLowerLeft(), super.getUpperRight()));
+                animal.reduceEnergy( portalEnergyConsumption);
+        }
+        else{
+            animal.setPosition(oldPosition);
+        }
     }
+    private boolean overBorder(Vector2d position){
+        return position.x > super.getUpperRight().x || position.x < super.getLowerLeft().x ||
+                position.y > super.getUpperRight().y || position.y < super.getLowerLeft().y;
+    }
+
 
 }
