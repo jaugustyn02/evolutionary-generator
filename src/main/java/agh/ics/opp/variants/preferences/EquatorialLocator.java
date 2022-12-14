7b3a998eca33a5b3d8d2279b4c475e65ec80a567
Green0x0y <66325429+Green0x0y@users.noreply.github.com>
2022-12-14 17:23:29 +0100

    Working on EquatorialLocator

--- src/main/java/agh/ics/opp/variants/preferences/EquatorialLocator.java
+++ src/main/java/agh/ics/opp/variants/preferences/EquatorialLocator.java
@@ -2,13 +2,19 @@ package agh.ics.opp.variants.preferences;
 
 import agh.ics.opp.Vector2d;
 import agh.ics.opp.variants.maps.IWorldMap;
-
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class EquatorialLocator implements IPlantPlacementLocator {
     private final Vector2d equatorLowerLeft, equatorUpperRight;
     final private IWorldMap map;
+    //liczba wszystkich pól na równiku i poza
+    private final int maxPlantsEquator;
+    private final int maxPlantsAround;
+    //liczba roślin na powyższych polach
+
+    private int numPlantsEquator = 0;
+    private int numPlantsAround = 0;
 
     public EquatorialLocator(IWorldMap map) {
         this.map = map;
@@ -16,6 +22,9 @@ public class EquatorialLocator implements IPlantPlacementLocator {
         this.equatorLowerLeft = new Vector2d(map.getLowerLeft().x, (int)((map.getUpperRight().y-map.getLowerLeft().y)*0.4));
         //upperRight.y - 60% height
         this.equatorUpperRight = new Vector2d(map.getUpperRight().x, (int)((map.getUpperRight().y-map.getLowerLeft().y)*0.6));
+
+        this.maxPlantsEquator = (equatorUpperRight.x - equatorLowerLeft.x)*(equatorUpperRight.x - equatorLowerLeft.x);
+        this.maxPlantsAround = map.getWidth()*map.getHeight() - this.maxPlantsEquator;
         System.out.println("equator: "+equatorLowerLeft+", "+equatorUpperRight);
     }
 
@@ -30,16 +39,18 @@ public class EquatorialLocator implements IPlantPlacementLocator {
         } while (map.isPlantAt(position));
         return position;
     }
-
-    // czy równik jest cały zapełniony
-    private boolean equatorIsFull(){
-        return true; // Trzeba wiedzieć ile jest niezajętych pól przez rośliny na równiku
-    }
-
     // czy mapa jest cała zapełniona
     private boolean mapIsFull(){
-        return false; // Trzeba wiedzieć ile jest niezajętych pól przez rośliny na całej mapie
+        return numPlantsEquator + numPlantsAround == maxPlantsEquator + maxPlantsAround;
     }
+    // czy równik jest cały zapełniony
+    private boolean equatorIsFull(){
+        return numPlantsEquator == maxPlantsEquator;
+        // Trzeba wiedzieć ile jest niezajętych pól przez rośliny na równiku
+    }
+
+
+
 
     // metoda musi sprawdzać czy w ogóle jest miejsce na umieszczenie kolejnej rośliny
     // jeżeli nie ma w ogóle na mapie, to możemy ustalić że zwracana jest wartość null
@@ -47,14 +58,20 @@ public class EquatorialLocator implements IPlantPlacementLocator {
         if (mapIsFull()) return null;
         // 20% chance to get position from hole map or if equator is full
         if (ThreadLocalRandom.current().nextInt(0, 5) == 0 || equatorIsFull()){
+            numPlantsAround++;
+            map.addCurrentPlantsNumber();
             return new Random().nextBoolean() ?
                     getRandomPositionFromArea(map.getLowerLeft(), new Vector2d(map.getUpperRight().x, equatorLowerLeft.y-1)) :
                     getRandomPositionFromArea(new Vector2d(map.getLowerLeft().x, equatorUpperRight.y+1), map.getUpperRight());
         }
         // 80% chance to get position from equator
         else{
+            numPlantsEquator++;
+            map.addCurrentPlantsNumber();
             return getRandomPositionFromArea(equatorLowerLeft, equatorUpperRight);
         }
     }
+
+   
 }
 
