fdb61af518baa5f9b3c7bef2452e738f984d2f61
Jan Augustyn <jan.augusty02@gmail.com>
2022-12-26 18:10:13 +0100

    StatisticsRunner

--- src/main/java/agh/ics/opp/simulation/SimulationEngine.java
+++ src/main/java/agh/ics/opp/simulation/SimulationEngine.java
@@ -10,16 +10,18 @@ public class SimulationEngine implements IEngine, Runnable{
     private final IWorldMap map;
     private final MapRenderer renderer;
     private final MapUpdater updater;
+    private final StatisticsRunner stats;
     private int moveDelay = 1000;
     private int dayNum = 0;
 
     private volatile boolean running = true;
     private volatile boolean paused = false;
 
-    public SimulationEngine(SimulationSetup setup, IWorldMap map, MapRenderer renderer) {
+    public SimulationEngine(SimulationSetup setup, IWorldMap map, MapRenderer renderer, StatisticsRunner stats) {
         this.updater = new MapUpdater(map, setup);
         this.map = map;
         this.renderer = renderer;
+        this.stats = stats;
         this.renderer.render();
         System.out.println(map);
     }
@@ -41,11 +43,13 @@ public class SimulationEngine implements IEngine, Runnable{
                         System.out.println("Exception: The simulation has been aborted (2)");
                     }
                 }
-                updater.nextDay();
                 dayNum++;
-                System.out.println("Day " + (dayNum) + ":");
+                updater.nextDay();
                 this.renderer.render();
+                stats.updateStatistics();
+                System.out.println("Day " + (dayNum) + ":");
                 System.out.println(map);
+                System.out.println(stats.getStatistics());
             }
         }
     }
